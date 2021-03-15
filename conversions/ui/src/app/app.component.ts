2e34917c3121bd0ec82d861ed41800145b1d6ebf
ipokunzi <peter.okunzi@gmail.com>
2021-03-15 14:43:38 +0200

    Add files via upload

--- conversions/ui/src/app/app.component.ts
+++ conversions/ui/src/app/app.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ApplicationService } from './services/application.service';
 
 @Component({
@@ -10,59 +10,92 @@ import { ApplicationService } from './services/application.service';
 export class AppComponent implements OnInit {
   title = 'UI';
   
-  squaremiles = {};
-  squarekilometers = {};
-  miles = {};
-  kilometers = {};
-  kilograms = {};
-  pounds = {};
-  celsius = {};
-  kelvin = {};
-  liters = {};
-  milliliters = {};
+  squaremiles = "";
+  squarekilometers = "";
+  miles = "";
+  kilometers = "";
+  kilograms = "";
+  pounds = "";
+  celsius = "";
+  kelvin = "";
+  liters = "";
+  milliliters = "";
+  
+  squarekilometers_msg:string;
+  squaremiles_msg:string;
+  miles_msg:string;
+  kilometers_msg:string;
+  kilograms_msg:string;
+  pounds_msg:string;
+  celsius_msg:string;
+  kelvin_msg:string;
+  liters_msg:string;
+  milliliters_msg:string;
 
   constructor(private applicationService: ApplicationService) {
   }
 
   ngOnInit() {
-    this.applicationService.getSquareMiles(1).subscribe((response: any) => {
-      	this.squaremiles = response;
-    })
     
-    this.applicationService.getSquareKilometers(1).subscribe((response: any) => {
-      	this.squarekilometers = response;
-    })
-    
-    this.applicationService.getMiles(1).subscribe((response: any) => {
-      	this.miles = response;
-    })
-    
-    this.applicationService.getKilometers(1).subscribe((response: any) => {
-      	this.kilometers = response;
-    })
-    
-    this.applicationService.getPounds(1).subscribe((response: any) => {
-      	this.pounds = response;
-    })
-    
-    this.applicationService.getKilograms(1).subscribe((response: any) => {
-      	this.kilograms = response;
-    })
-    
-    this.applicationService.getKelvin(0).subscribe((response: any) => {
-      	this.kelvin = response;
-    })
-    
-    this.applicationService.getCelsius(0).subscribe((response: any) => {
-      	this.celsius = response;
-    })
-    
-    this.applicationService.getLiters(1).subscribe((response: any) => {
-      	this.liters = response;
-    })
-    
-    this.applicationService.getMilliliters(1).subscribe((response: any) => {
-      	this.milliliters = response;
-    })
+  }
+
+  clickEventSquareKilometers(){
+    	this.applicationService.getSquareMiles(this.squarekilometers_msg).subscribe((response: any) => {
+          	this.squaremiles = response;
+        })
+  }
+  
+  clickEventSquareMiles(){
+	  this.applicationService.getSquareKilometers(this.squaremiles_msg).subscribe((response: any) => {
+	    	this.squarekilometers = response;
+	  })
+  }
+  
+  clickEventKilometers(){
+	  this.applicationService.getMiles(this.kilometers_msg).subscribe((response: any) => {
+	    	this.miles = response;
+	  })
+  }
+  
+  clickEventMiles(){
+	  this.applicationService.getKilometers(this.miles_msg).subscribe((response: any) => {
+	    	this.kilometers = response;
+	  })
+  }
+  
+  clickEventKilograms(){
+	  this.applicationService.getPounds(this.kilograms_msg).subscribe((response: any) => {
+	    	this.pounds = response;
+	  })
+  }
+  
+  clickEventPounds(){
+	  this.applicationService.getKilograms(this.pounds_msg).subscribe((response: any) => {
+	    	this.kilograms = response;
+	  })
+  }
+  
+  clickEventCelsius(){
+	  this.applicationService.getKelvin(this.celsius_msg).subscribe((response: any) => {
+	    	this.kelvin = response;
+	  })
+  }
+  
+  clickEventKelvin(){
+	  this.applicationService.getCelsius(this.kelvin_msg).subscribe((response: any) => {
+	    	this.celsius = response;
+	  })
+  }
+  
+  clickEventMilliliters(){
+	  this.applicationService.getLiters(this.milliliters_msg).subscribe((response: any) => {
+	    	this.liters = response;
+	  })
+  }
+  
+  clickEventLiters(){
+	  this.applicationService.getMilliliters(this.liters_msg).subscribe((response: any) => {
+	    	this.milliliters = response;
+	  })
   }
 }
--- conversions/ui/src/app/app.module.ts
+++ conversions/ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { ApplicationService } from './services/application.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ApplicationService],
   bootstrap: [AppComponent]
